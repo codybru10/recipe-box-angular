b9f7d3f5403a3abbcc9e23a3ed1e7a5f64b878b1
Cody Brubaker <cody.brubaker@gmail.com>
2017-01-17 18:24:57 -0800

    finish recipe box

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,10 +5,25 @@ import { Component } from '@angular/core';
   template: `
   <div class="container">
     <h1>Recipe Box</h1>
+    <h3>{{type}}</h3>
+    <div class="well" *ngFor="let currentRecipe of recipies">
+      <h4>{{currentRecipe.title}}</h4>
+      <h5>{{currentRecipe.ingredients}}</h5>
+      <p>{{currentRecipe.directions}}</p>
+    </div>
   </div>
   `
 })
 
 export class AppComponent {
+  type: string = 'Entrees';
+  recipies: Recipe[] = [
+    new Recipe('Baked Chicken', 'Chicken and lots of butter', 'Throw ingredients in the over at 350'),
+    new Recipe('Alfredo', 'Noodle and Alfredo Sauce', 'Boil noodles until al dente and miss with sauce'),
+    new Recipe('Steak', 'Lean angus', 'Throw it on the grill')
+  ];
+}
 
+export class Recipe {
+  constructor(public title: string, public ingredients: string, public directions: string) {}
 }
